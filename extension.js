0599f625fbb1f65d81b5e66e86fea975b4d62c52
David Liang <liangchenye@gmail.com>
2011-08-19 13:52:06 +0800

    clean app_store

--- extension.js
+++ extension.js
@@ -44,38 +44,14 @@ MyAppWellIcon.prototype = {
     }
 }
 
-function _showHello() {
-    let text = new St.Label({ style_class: 'helloworld-label', text: "David said: Harmony world!" });
-    let monitor = global.get_primary_monitor();
-    global.stage.add_actor(text);
-    text.set_position(Math.floor (monitor.width / 2 - text.width / 2), Math.floor(monitor.height / 2 - text.height / 2));
-    Mainloop.timeout_add(3000, function () { text.destroy(); });
-}
-
 // Put your extension initialization code here
 function main() {
-    Main.panel.actor.reactive = true;
-    Main.panel.actor.connect('button-release-event', _showHello);
-
-  
-   let hotCornerButton = Main.panel.button;
-
-   let logo = new St.Icon({ icon_type: St.IconType.FULLCOLOR, icon_size: hotCornerButton.height, icon_name: 'suse' });
-   let box = new St.BoxLayout();
-   let text = new St.Label({ style_class: 'corn-label', text: "dl" });
-   box.add_actor(text);
-   box.add_actor(logo);
-   Main.panel.button.set_child(box);
-
 //-------------------app store -----------
-   let logo1 = new St.Icon({ icon_type: St.IconType.FULLCOLOR, icon_size: hotCornerButton.height, icon_name: 'suse' });
    let box1 = new St.BoxLayout({ vertical: true });
    let grid = new IconGrid.IconGrid({ xAlign: St.Align.START });
    box1.add(grid.actor, { y_align: St.Align.START, expand: true });
-   grid.addItem (logo1);
 
    let store = new Store.AppStore ();
-   let i;
    for (let i=0; i<20; i++) {
 	let id = store.get_nth_app (i);
 	let name = store.get_name_from_id (id);
